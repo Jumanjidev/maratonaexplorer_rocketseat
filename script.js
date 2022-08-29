5205a375455aa8c83694323179e2bfc8539ec6fb
Jumanjidev <leandro.sipriano@ufpe.br>
2022-08-28 23:53:08 -0300

    First Commit
    Projeto Front-End de Menu de Cafeteria da Maratona Explorer Rocketseat

---
+++ script.js
@@ -0,0 +1,10 @@
+/* JavaScript para fazer Carousel com Slick */
+ 
+$('.carousel').slick({
+    dots: true,
+    infinite: false,
+    speed: 200,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    adaptiveHeight: true
+});
